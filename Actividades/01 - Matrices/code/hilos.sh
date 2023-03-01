ec304e72eeb2c0a2d138bc8c5655c0974901cde6
Sebastián Buitrago <sebastianbuitrago0107@gmail.com>
2023-03-01 15:49:16 -0500

    Código de testeo

---
+++ Actividades/01 - Matrices/code/hilos.sh
@@ -0,0 +1,8 @@
+for size in 500 1000 1500 2000 2500 3000 3500 4000 4500 5000; do
+    for threads in 2 4 8 16; do
+        echo "------------------" $size $threads >>../Results/'Threads.csv'
+        for i in {1..10}; do
+            .././hilos $size $threads >>../Results/'Threads.csv'
+        done
+    done
+done
