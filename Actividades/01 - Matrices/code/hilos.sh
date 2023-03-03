950c06f564da1ac7b6503ce2f60dbd1dcb881942
Sebastián Buitrago <sebastianbuitrago0107@gmail.com>
2023-03-03 18:18:14 -0500

    Pruebas beta

--- Actividades/01 - Matrices/code/hilos.sh
+++ Actividades/01 - Matrices/code/hilos.sh
@@ -1,5 +1,5 @@
-for size in 500 1000 1500 2000 2500 3000 3500 4000 4500 5000; do
-    for threads in 2 4 8 16; do
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
+    for threads in 2 4; do
         echo "------------------" $size $threads >>../Results/'Threads.csv'
         for i in {1..10}; do
             .././hilos $size $threads >>../Results/'Threads.csv'
--- Actividades/01 - Matrices/code/secuencial.sh
+++ Actividades/01 - Matrices/code/secuencial.sh
@@ -1,4 +1,4 @@
-for size in 500 1000 1500 2000 2500 3000 3500 4000 4500 5000; do
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
     echo "------------------" $size >>../Results/'Sequential.csv'
     for i in {1..10}; do
         .././secuencial $size >>../Results/'Sequential.csv'
