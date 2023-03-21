701216325ad2dfc3a42a952cf2669a0b8526caec
Juan Esteban Pescador <juanesteban.pescador@utp.edu.co>
2023-03-21 12:05:49 -0500

    Estructura del testeo

--- Actividades/01 - Matrices/tests/test.sh
+++ Actividades/01 - Matrices/tests/test.sh
@@ -1,38 +1,54 @@
-#It make every iteration in each file first. God damn...
+#serial processing
+: 'echo "Serial test in progress..."
 for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
-
-    #serial processing
-    echo "Serial test in progress..."
     echo "------------------" $size >>results/'Serial.csv'
     for i in {1..10}; do
         run/./serial $size >>results/'Serial.csv'
     done
-    echo "done!"
+done
+echo "done!"
+'
 
-    #parallel processing
-    echo "Parallel test in progress..."
+#parallel processing
+: 'echo "Parallel test in progress..."
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
     for threads in 2 4 8; do
         echo "------------------" $size $threads >>results/'Threads.csv'
         for i in {1..10}; do
             run/./threads $size $threads >>results/'Threads.csv'
         done
     done
-    echo "done!"
+done
+echo "done!"
+'
 
-    #cache memory line optimization
-    echo "Optimized by memory test in progress..."
+#cache memory line optimization
+echo "Optimized by memory test in progress..."
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
     echo "------------------" $size >>results/'Memory.csv'
     for i in {1..10}; do
         run/./optimize $size >>results/'Memory.csv'
     done
-    echo "done!"
+done
+echo "done!"
 
-    #optimized by compiler
-    echo "Optimized by compiler test in progress..."
+#optimized by compiler
+echo "Optimized by compiler test in progress..."
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
     echo "------------------" $size >>results/'Compiler.csv'
     for i in {1..10}; do
         run/./o3.out $size >>results/'Compiler.csv'
     done
-    echo "done!"
+done
+echo "done!"
 
+#Optimized by processes (?)
+echo "Optimized by processes test in progress..."
+for size in 500 1000 1500 2000 2500 3000 3500 4000 5000; do
+    for processes in 2 4 8; do
+        echo "------------------" $size $processes >>results/'Processes.csv'
+        for i in {1..10}; do
+            run/./processes $size $processes >>results/'Processes.csv'
+        done
+    done
 done
