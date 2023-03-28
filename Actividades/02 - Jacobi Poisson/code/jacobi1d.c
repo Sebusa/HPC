9ac3dbf8375d21b93bc2746a15fd357636e6bcef
Sebastián Buitrago <sebastianbuitrago0107@gmail.com>
2023-03-27 23:07:41 -0500

    Refacto y bloques de comentarios

--- Actividades/02 - Jacobi Poisson/code/jacobi1d.c
+++ Actividades/02 - Jacobi Poisson/code/jacobi1d.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "timing.h"
 
-/* --
- * Do nsweeps sweeps of Jacobi iteration on a 1D Poisson problem
- *
- *    -u'' = f
- *
- * discretized by n+1 equally spaced mesh points on [0,1].
- * u is subject to Dirichlet boundary conditions specified in
- * the u[0] and u[n] entries of the initial vector.
- */
+/* Function to perform nsweeps sweeps of Jacobi iteration on a 1D Poisson problem */
 void jacobi(int nsweeps, int n, double *u, double *f)
 {
+    /* Declare variables */
     int i, sweep;
     double h = 1.0 / n;
     double h2 = h * h;
@@ -24,9 +16,9 @@ void jacobi(int nsweeps, int n, double *u, double *f)
     utmp[0] = u[0];
     utmp[n] = u[n];
 
+    /* Perform Jacobi iteration */
     for (sweep = 0; sweep < nsweeps; sweep += 2)
     {
-
         /* Old data in u; new data in utmp */
         for (i = 1; i < n; ++i)
             utmp[i] = (u[i - 1] + u[i + 1] + h2 * f[i]) / 2;
@@ -36,57 +28,67 @@ void jacobi(int nsweeps, int n, double *u, double *f)
             u[i] = (utmp[i - 1] + utmp[i + 1] + h2 * f[i]) / 2;
     }
 
+    /* Free memory allocated to utmp */
     free(utmp);
 }
 
+/* Function to write the solution to a file */
 void write_solution(int n, double *u, const char *fname)
 {
+    /* Declare variables */
     int i;
     double h = 1.0 / n;
     FILE *fp = fopen(fname, "w+");
+
+    /* Write solution to file */
     for (i = 0; i <= n; ++i)
         fprintf(fp, "%g %g\n", i * h, u[i]);
+
+    /* Close file */
     fclose(fp);
 }
 
 int main(int argc, char **argv)
 {
+    /* Declare variables */
     int i;
     int n, nsteps;
-    double *u;
-    double *f;
+    double *u, *f;
     double h;
     timing_t tstart, tend;
     char *fname;
 
-    /* Process arguments */
+    /* Process command line arguments */
     n = (argc > 1) ? atoi(argv[1]) : 100;
     nsteps = (argc > 2) ? atoi(argv[2]) : 100;
     fname = (argc > 3) ? argv[3] : NULL;
     h = 1.0 / n;
 
-    /* Allocate and initialize arrays */
+    /* Allocate memory for arrays */
     u = (double *)malloc((n + 1) * sizeof(double));
     f = (double *)malloc((n + 1) * sizeof(double));
+
+    /* Initialize arrays */
     memset(u, 0, (n + 1) * sizeof(double));
     for (i = 0; i <= n; ++i)
         f[i] = i * h;
 
-    /* Run the solver */
+    /* Perform Jacobi iteration */
     get_time(&tstart);
     jacobi(nsteps, n, u, f);
     get_time(&tend);
 
-    /* Run the solver */
+    /* Print results */
     printf("n: %d\n"
-           "nsteps: %d\n"
-           "Elapsed time: %g s\n",
-           n, nsteps, timespec_diff(tstart, tend));
+            "nsteps: %d\n"
+            "Elapsed time: %g s\n",
+            n, nsteps, timespec_diff(tstart, tend));
 
-    /* Write the results */
+    /* Write solution to file */
     if (fname)
         write_solution(n, u, fname);
 
+    /* Free memory allocated for arrays */
     free(f);
     free(u);
     return 0;
